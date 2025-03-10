bdc6aa237cea1dab555ca51da226fbb6d4d380a1
tanver909 <tanveermahidi@gmial.com>
2025-03-10 15:36:09 +0600

    react router installed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl font-bold'>
+        Team WebForge
+      </h1>
+    </>
+  )
+}
+
+export default App
